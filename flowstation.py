006252c7b7379f3b054326719b4ef66a385c8b9b
unknown <tlavelle@GRSLW40011678.ndc.nasa.gov>
2013-01-23 10:30:25 -0500

    get alpha case running

--- flowstation.py
+++ flowstation.py
@@ -3,62 +3,100 @@ from openmdao.lib.datatypes.api import Float
 from scipy.optimize import newton
 
 from Cantera import *
+from secant import*
 #from pylab import *
 
 #justin added comment here
-reactants=[]
-reactants.append( "Air" )
-reactants.append( "H2O" )
 
 class CanteraFS(VariableTree):
 	
-	ht = Float(0.0, iotype='in', desc='' )
-	area = Float(0.0, iotype='in', desc='')
-	Tt = Float(0.0, iotype='in', desc='')
-	Pt = Float(0.0, iotype='in', desc='')
-	rhot = Float(0.0, iotype='in', desc='') 
-	gamt = Float(0.0, iotype='in', desc='') 
-	s  = Float(0.0, iotype='in', desc='')
-	W  = Float(0.0, iotype='in', desc='') 
-	FAR  = Float(0.0, iotype='in', desc='') 
-	WAR  = Float(0.0, iotype='in', desc='') 
-	hs = Float(0.0, iotype='in', desc='')
-	Ts = Float(0.0, iotype='in', desc='')
-	Ps = Float(0.0, iotype='in', desc='')
-	rhos = Float(0.0, iotype='in', desc='')
-	gams = Float(0.0, iotype='in', desc='')    
-	Vflow  = Float(0.0, iotype='in', desc='')   
-	Mach = Float(0.0, iotype='in', desc='')
-	area  = Float(0.0, iotype='in', desc='') 
+	
+	reactants=["Air","H2O" ]
 
+	ht = Float(     0.0, iotype='in', desc='total enthalpy', unit = 'BTU/lbm' )
+	Tt = Float(     0.0, iotype='in', desc='total temperature', unit = 'R' )
+	Pt = Float(     0.0, iotype='in', desc='total pressure', unit = 'lbf/in2' )
+	rhot = Float(   0.0, iotype='in', desc='total density', unit = 'lbm/ft3' ) 
+	gamt = Float(   0.0, iotype='in', desc='total gamma', unit = '' ) 
+	s  = Float(     0.0, iotype='in', desc='entropy', unit = 'BTU/lbm-R' )
+	W  = Float(     0.0, iotype='in', desc='weight flow', unit = '' ) 
+	FAR  = Float(   0.0, iotype='in', desc='fuel to air ratio', unit = '' ) 
+	WAR  = Float(   0.0, iotype='in', desc='water to air ratio', unit = '' ) 
+	hs = Float(     0.0, iotype='in', desc='static enthalpy', unit = 'BTU/lbm' )
+	Ts = Float(     0.0, iotype='in', desc='static temperature', unit = 'R' )
+	Ps = Float(     0.0, iotype='in', desc='static pressure', unit = 'lbf/in2' )
+	rhos = Float(   0.0, iotype='in', desc='static density', unit = 'lbm/ft3' )
+	gams = Float(   0.0, iotype='in', desc='static gamma', unit = '' )    
+	Vflow  = Float( 0.0, iotype='in', desc='Velocity', unit = 'ft/sec' )   
+	Mach = Float(   0.0, iotype='in', desc='Mach number', unit = '' )
+	area  = Float(  0.0, iotype='in', desc='flow area', unit = 'in2' ) 
+
+        trigger = int( 0 )
+        
+        #add a reactant that can be mixed in
+	def add_reactant( self, reactant ):
+		for i in range(0,len(self.reactants)):
+			if self.reactants[i] == reactant:
+				return
+		self.reactants.append( reactant )
+		
+	#intialize station	
 	def __init__(self): 
 		self._species=[1.0,0,0,0,0,0,0,0]
 		self._mach_or_area = 0    
-		self._flow = importPhase( 'gri50tom.cti' )
-		self._flowS = importPhase( 'gri50tom.cti' )
-	
+		self._flow = importPhase( 'gri1000.cti' )
+		self._flowS = importPhase( 'gri1000.cti' )
+		self.setDryAir()
+		
+	#trigger action on Mach
 	def _Mach_changed(self):
-		self._mach_or_area = 1
+		if self.trigger == 0:
+			self.trigger = 1
+			self._mach_or_area = 1
+			self.setStatic()
+			self.trigger = 0
+			
+	#trigger action on area        
+	def _area_changed(self):
+		if self.trigger == 0:
+			self.trigger  = 1
+			self._mach_or_area = 2
+			self.setStatic()
+			self.trigger = 0
+	       
+	#trigger action on static pressure       
+	def _Ps_changed(self):
+		if self.trigger == 0:
+			self.trigger = 1
+			self._mach_or_area = 3
+			self.setStatic()
+			self.trigger = 0 
 	
-	def _area_changed(self, old, new):
-		self._mach_or_area = 2
-	
-	
+	#set the composition to dry air
 	def setDryAir( self ):
 		self._flow.setMassFractions("Air:1.00") 	    
-		self._species = len(reactants)*[0,]
+		self._species = (len(self.reactants)+1)*[0,]
 		self._species[0]=1 
+		self.WAR = 0
+		self.FAR = 0
 		self.setStatic()
+		self.trigger = 0
 	
+	#set the compositon to air with water
 	def setWAR( self, WAR ):
+		self.trigger = 1
 		self.WAR = WAR
+		self.FAR = 0
 		self._flow.setMassFractions("Air:"+str((1-WAR)/(1+WAR))+" H2O:"+str((WAR)/(1+WAR)))
-		self._species = len(reactants)*[0,]
+		self._species = len(self.reactants)*[0,]
 		self._species[0]=(1-WAR)/(1+WAR)
 		self._species[1]=(WAR)/(1+WAR)
 		self.setStatic()
+		self.trigger = 0
     
+    	#set total conditions based on T an P
 	def setTotalTP( self, Tin, Pin ):
+		self.trigger = 1
 		self.Tt = Tin
 		self.Pt = Pin    	    
 		self._flow.set(T=Tin*5./9.,P=Pin*6894.75729)
@@ -70,8 +108,11 @@ class CanteraFS(VariableTree):
 		self.gamt = self._flow.cp_mass()/self._flow.cv_mass()
 		self._flowS = self._flow 
 		self.setStatic()
-	
+		self.trigger = 0
+
+	#set total conditions based on h and P
 	def setTotal_hP( self, hin, Pin ):
+		self.trigger = 1 
 		self.ht = hin
 		self.Pt = Pin
 		self._flow.set(H=hin/.0004302099943161011,P=Pin*6894.75729)
@@ -81,9 +122,12 @@ class CanteraFS(VariableTree):
 		self.rhot = self._flow.density()*.0624
 		self.gamt = self._flow.cp_mass()/self._flow.cv_mass()
 		self.setStatic()
-	
+		self.trigger = 0
+
+	#set total condition based on S and P
 	def setTotalSP( self, sin, Pin ):
-		self.S = sin
+		self.trigger = 1
+		self.s = sin
 		self.Pt = Pin 	    
 		self._flow.set(S=sin/0.000238845896627,P=Pin*6894.75729)
 		self._flow.equilibrate('SP' )
@@ -92,7 +136,10 @@ class CanteraFS(VariableTree):
 		self.rhot = self._flow.density()*.0624
 		self.gamt = self._flow.cp_mass()/self._flow.cv_mass()
 		self.setStatic()
-	
+		self.trigger = 0
+
+	#add another station to this one
+	#mix enthalpies and keep pressure and this stations value
 	def add( self, FS2 ):
 		temp =""
 		for i in range(0,len(reactants)):
@@ -109,23 +156,24 @@ class CanteraFS(VariableTree):
 			self.rhot = self._flow.density()*.0624
 			self.gamt = self._flow.cp_mass()/self._flow.cv_mass()          
 		
+	#burn a fuel with this station	
 	def burn( self, fuel, Wfuel, hfuel ):
 		flow_1 = self.W
 		ht = self.ht
 		self.W = self.W + Wfuel 
-		for i in range(1,len(reactants) ):
-			if fuel == reactants[i]:
+		for i in range(1,len(self.reactants) ):
+			if fuel == self.reactants[i]:
 				self._species[i]=( flow_1*self._species[i]+Wfuel )/ self.W
 			else:
 				self._species[i]=( flow_1*self._species[i] )/ self.W
 			
    		ht = ( flow_1 * ht + Wfuel * hfuel )/ self.W
-   
+                self.ht = ht
    		air1 = flow_1 * ( 1. / ( 1. + self.FAR + self.WAR ));
    		self.FAR = ( air1 * self.FAR + Wfuel )/( air1  );
    		temp =""
-   		for i in range(0,len(reactants)):
-   			temp = temp+reactants[i]+":"+str(self._species[i])+" "
+   		for i in range(0,len(self.reactants)):
+   			temp = temp+self.reactants[i]+":"+str(self._species[i])+" "
    			self._flow.setMassFractions(temp)	
    			self._flow.set(H=ht/0.0004302099943161011,P=self.Pt*6894.75729)
    			self._flow.equilibrate('HP' )
@@ -134,11 +182,10 @@ class CanteraFS(VariableTree):
    			self.rhot = self._flow.density()*.0624
    			self.gamt = self._flow.cp_mass()/self._flow.cv_mass() 
 	   
+        #set the statics based on Mach
 	def setStaticMach( self ):
 		self.MachTemp = 0
 		self.Ps = self.Pt*( 1 + ( self.gamt -1 )/2*self.Mach**2)**(self.gamt/( 1 - self.gamt ) )
-		
-                print "Pressures " + str( self.Ps ) + " " + str ( self.Pt ) 
 		def eval(Ps):
 			self.Ps = Ps
 			self._flowS = self._flow 
@@ -147,30 +194,57 @@ class CanteraFS(VariableTree):
 			self.Ts = self._flowS.temperature()*9./5.
 			self.rhos = self._flowS.density()*.0624
 			self.gams = self._flowS.cp_mass()/self._flowS.cv_mass() 
-			self.hs = self._flow.enthalpy_mass()*0.0004302099943161011 	          
+			self.hs = self._flowS.enthalpy_mass()*0.0004302099943161011 	          
 			Vson = math.sqrt(self.gams*GasConstant*self._flowS.temperature()/ self._flowS.meanMolecularWeight() )*3.28084
 			self.Vflow = math.sqrt(  778.169 * 32.1740 * 2 * ( self.ht - self.hs ) )
 			self.MachTemp = self.Vflow / Vson
 			return self.Mach - self.MachTemp
-		newton(eval, self.Ps)
-		
+		secant(eval, self.Ps-.1, self.Ps)
+
+	#set the statics based on pressure
+	def setStaticPs( self ):
+		self._flowS = self._flow 
+		self._flowS.set(S=self.s/0.000238845896627,P=self.Ps*6894.75729)
+		self._flowS.equilibrate('SP' )
+		self.Ts = self._flowS.temperature()*9./5.
+		self.rhos = self._flowS.density()*.0624
+		self.gams = self._flowS.cp_mass()/self._flowS.cv_mass() 
+		self.hs = self._flowS.enthalpy_mass()*0.0004302099943161011 	          
+		Vson = math.sqrt(self.gams*GasConstant*self._flowS.temperature()/ self._flowS.meanMolecularWeight() )*3.28084
+		self.Vflow = math.sqrt(  778.169 * 32.1740 * 2 * ( self.ht - self.hs ) )
+		self.Mach = self.Vflow / Vson
+		self.area =  self.W / ( self.rhos * self.Vflow )*144. 
+
+	#determine which static calc to use
 	def setStatic(self):
-		print "Check: ", self._mach_or_area
 		if self._mach_or_area  == 0:
-			print "Nieher Mach or Area has been set"
-			return
+			pass
 		elif self._mach_or_area == 1:
 			self.setStaticMach()
-			self.area =  self.W / ( self.rhos * self.Vflow ) 
+			self.area =  self.W / ( self.rhos * self.Vflow )*144. 
 		elif self._mach_or_area ==2:
-			Mach = .5
+			Mach = .45
 			def F1(Mach):
 				self.Mach = Mach
 				self.setStaticMach( )
-			        return  self.W / ( self.rhos * self.Vflow ) - self.area
+			        return  self.W / ( self.rhos * self.Vflow )*144. - self.area		
+			secant( F1, Mach+.05, Mach, MAXDX = .1 )
+		elif self._mach_or_area == 3:
+			self.setStaticPs()
+
 		
-			newton( F1, Mach )
-			print "area calc", self.area
+	#set the statics based on Ts, Ps, and MN
+	#UPDGRAEDE TO USE LOOPS
+	def setStaticTsPsMN(self, Ts, Ps, MN ): 
+		self.trigger = 1 
+		self.Tt = Ts*(1+( self.gamt - 1 ) /2.* MN**2 )
+		self.Pt = Ps*( 1+( self.gamt - 1 ) /2.* MN**2 )**( self.gamt /( self.gamt -1 ))
+		self.setTotalTP( self.Tt, self.Pt )
+		self.trigger = 1
+		self.Mach = MN 
+		self.setStaticMach()
+		self.area =  self.W / ( self.rhos * self.Vflow )*144. 
+		self.trigger = 0
 				
 				
 				
--- test_flowstation.py
+++ test_flowstation.py
@@ -2,13 +2,13 @@
 
 import unittest
 
-from flowstation import CanteraFlowStation
+from flowstation import *
 
 class TestSetTotals(unittest.TestCase): 
 
     def setUp(self): 
         """Initialization function called before every test function""" 
-        self.fs = CanteraFlowStation()
+        self.fs = CanteraFS()
         self.fs.W = 100
         self.fs.setTotalTP(518,15)
 
@@ -18,77 +18,113 @@ class TestSetTotals(unittest.TestCase):
         pass #nothing to do for this test
 
      #all test function have to start with "test_" as the function name
-     def test_setTotalTP(self):
-
-        self.assertEqual(self.fs.Pt,15.0)
-        self.assertEqual(self.fs.Tt,518)
-        self.assertEqual(self.fs.ht, -6.34675) #Tom says the ht values will be different
-        self.assertEqual(self.fs.W,100)
-
-     def test_setTotal_hP(self):
+    def test_setTotalTP(self):
+        self.assertAlmostEqual(self.fs.Pt,15.0,places = 2 )
+        self.assertAlmostEqual(self.fs.Tt,518,places = 2)
+        self.assertAlmostEqual(self.fs.ht, -6.76639,places = 4 ) #Tom says the ht values will be different
+        self.assertAlmostEqual(self.fs.W,100,places = 2)
+        self.assertAlmostEqual(self.fs.rhot,.07812,places = 4)
+ 
+    def test_setTotal_hP(self):
         ht = self.fs.ht
-
         self.fs.setTotalTP(1000,40) #just to move things around a bit
         self.fs.setTotal_hP(ht,15)
-
         self.test_setTotalTP() #just call this since it has all the checks we need  
 
-    def test_setTotal_hP(self):
-        S = self.fs.S
-
+    def test_setTotal_SP(self):
+        s = self.fs.s
         self.fs.setTotalTP(1000,40) #just to move things around a bit
-        self.fs.setTotalSP(S,15)
-        
+        self.fs.setTotalSP(s,15)      
         self.test_setTotalTP() #just call this since it has all the checks we need  
      
+    def test_delh(self):
+    	ht = self.fs.ht
+    	self.fs.setTotalTP( 1000, 40 )
+    	diffh = self.fs.ht - ht
+        self.assertAlmostEqual(diffh,117.4544,places = 2)    	
+        
+    def test_dels(self):
+    	s = self.fs.s
+    	self.fs.setTotalTP( 1000, 40 )
+    	diffs = self.fs.s - s
+        self.assertAlmostEqual(diffs,.092609,places = 4)          
+        
     def test_set_WAR(self):
         self.fs.WAR = 0.02
         self.fs.setTotalTP(1000,15) 
 
 
-        self.assertEqual(self.fs.Pt,15.0)
-        self.assertEqual(self.fs.Tt,1000)
-        self.assertEqual(self.fs.ht, -0.259263) #Tom says the ht values will be different
-        self.assertEqual(self.fs.W,100)
+        self.assertAlmostEqual(self.fs.Pt,15.,places = 2)
+        self.assertAlmostEqual(self.fs.Tt,1000,places = 2)
+        self.assertAlmostEqual( self.fs.WAR, 0.02,places = 2 )
+        self.assertAlmostEqual( self.fs.FAR, 0,places = 2 )
 
+    def test_setDryAir(self):
+        self.fs.setDryAir()
+        self.assertAlmostEqual( self.fs.WAR, 0,places = 2 )
+        self.assertAlmostEqual( self.fs.FAR, 0,places = 2 )
+        
 class TestBurn(unittest.TestCase): 
     def setUp(self):
-        pass
+        self.fs = CanteraFS()
+        self.fs.setDryAir()
+        self.fs.setTotalTP(1100, 400 )
+        self.fs.W = 100.
+        self.fs.add_reactant( "Jet-A(g)")
+        self.fs.burn( "Jet-A(g)", 2.5, 0 )  
 
     #all test cases use the same checks here, so just re-use
     def _assert(self): 
+        print (self.fs._flow)  
+        self.assertAlmostEqual(self.fs.W,102.5,places = 2 )
+        self.assertAlmostEqual( self.fs.FAR, .025,places = 2 )   
+        self.assertAlmostEqual(self.fs.Pt,400,places = 2)
+        self.assertAlmostEqual(self.fs.Tt,2685.5,places = 0)
+        self.assertAlmostEqual(self.fs.ht,132.4,places = 1) 
+        self.assertAlmostEqual(self.fs.rhot,.401845715,places = 2)
+        self.assertAlmostEqual(self.fs.W,102.5,places=4)
+        self.assertAlmostEqual(self.fs.gamt,1.293,places=3)
 
-        self.assertEqual(self.fs.Pt,250)
-        self.assertEqual(self.fs.Tt,3883.35)
-        self.assertEqual(self.fs.ht,-0.246841) #Tom says the ht values will be different
-        self.assertEqual(self.fs.W,105)
 
-    def test_burn(self):
-        #do some stuff
+    def test_burn(self):   	  
+        self._assert()
+        
+class TestStatics(unittest.TestCase): 
+    def setUp(self):
+        self.fs = CanteraFS()
+        self.fs.W = 100.
+        self.fs.setDryAir()
+        self.fs.setTotalTP(1100, 400 )
+       
 
+    #all test cases use the same checks here, so just re-use
+    def _assert(self): 
+ 
+        self.assertAlmostEqual(self.fs.area,32.0066,places = 1 )  
+    	self.assertAlmostEqual(self.fs.Mach,.3,places = 1 )   
+        self.assertAlmostEqual(self.fs.Ps,376.219,places = 1 )
+        self.assertAlmostEqual(self.fs.Ts, 1081.732, places = 0 )   
+        self.assertAlmostEqual(self.fs.Vflow,479.298,places = 0 )
+        self.assertAlmostEqual(self.fs.rhos,.9347,places = 2 )       
+        self.assertAlmostEqual(self.fs.Mach,.3,places = 2 )   
+        
+    def test_set_Mach( self ):
+    	self.fs.Mach = .3
+    	self._assert()
+
+    def test_set_area( self ):
+        self.fs.area = 32.0066
         self._assert()
 
-    def test_vitiated_TP(self):
-        #do some stuff
-
+    def test_set_Ps( self ):
+        self.fs.Ps = 376.219
         self._assert()
-
-    def test_vitiated_hP(self):
-        #do some stuff
-
+        
+    def test_setStaticTsPsMN( self ):
+        self.fs.setStaticTsPsMN( 1081.802, 376.219, .3 )
         self._assert()
-
-    def test_vitiated_SP(self):
-        #do some stuff
-
-        self._assert()
-
-
-class TestSetStatic(unittest.TestCase): 
-    pass        
-    
-
-
+        
+        
 #this code runs the test when you call the file 
 if __name__ == '__main__': 
     unittest.main()
