d09ff19105463b8e855a46a6be3ebacfceb88761
cheesie67 <Thomas.M.Lavelle@nasa.gov>
2013-04-16 13:25:44 -0400

    Update for beta release

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 from openmdao.main.api import Assembly
-from duct import Duct
-
+from simple import *
 
 class BasicModel(Assembly):
     """A basic OpenMDAO Model"""
@@ -14,14 +13,15 @@ class BasicModel(Assembly):
         # Create Duct component instances
         self.add('Duct2', Duct())
 
+
         # Add to driver's workflow
         self.driver.workflow.add('Duct1')
         self.driver.workflow.add('Duct2')
-        self.connect("Duct1.FSout", "Duct2.FSin")
+        self.connect("Duct1.FSout","Duct2.FSin")
         
 Model = BasicModel()
 Model.Duct1.dP=.05
 Model.Duct2.dP=.05
 Model.run()
-print "Exit Pressure " + str(Model.Duct1.FSout.Pt) + " " + str(Model.Duct1.FSin.Pt)+" " +str(Model.Duct1.dP)
-print "Exit Pressure " + str(Model.Duct2.FSout.Pt) + " " + str(Model.Duct2.FSin.Pt)+" " +str(Model.Duct2.dP)
+print "Exit Pressure " + str( Model.Duct1.FSout.Pt ) + " " + str( Model.Duct1.FSin.Pt )+" " +str(Model.Duct1.dP )
+print "Exit Pressure " + str( Model.Duct2.FSout.Pt ) + " " + str( Model.Duct2.FSin.Pt )+" " +str(Model.Duct2.dP )
